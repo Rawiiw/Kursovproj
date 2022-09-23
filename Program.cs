8620b0543f1142b143567434d7defd3269d0daf0
Rawiiw <79237095+Rawiiw@users.noreply.github.com>
2022-09-23 13:10:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using pob;
+
+namespace Pobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(pob.pobot());
+        }
+    }
+}
